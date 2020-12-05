678ccb3b2845ad0f48fc2c688b6e976d3224a436
Juan <1213@holbertonschool.com>
2020-12-05 17:47:12 -0500

    Desarrollo Web VueJS reto 2

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NewsApi></NewsApi>
+    <section class="container-fluid" id="team">
+      <div class="row justify-content-center mb-5">
+        <div class="col mt-5" v-for="(member, index) of team" :key="index">
+          <team-card :member="member"></team-card>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NewsApi from "./components/NewsApi";
+import TeamCard from "./components/TeamCard.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    NewsApi,
+    TeamCard,
+  },
+  data() {
+    return {
+      team: [
+        {
+          codigo: 1,
+          nombre: "Paola Hormaza",
+          descripcion:
+            'Contadora Publica de la Ciudad de Pasto, "La programación está inmersa en muchos ámbitos de la contabilidad una razón más para adquirir un nuevo conocimiento"',
+          image: "img/paola.PNG",
+          rol: "Desarrolladora FrontEnd",
+        },
+        {
+          codigo: 2,
+          nombre: "Kevin Figueroa",
+          descripcion:
+            "Estudiante de la Universidad Tecnológica de Bolívar, en la carrera de ingeniería de sistemas, 18 años, le gusta el mar, los atardeceres y disfruta de una buena película, se caracteriza por ser una persona calmada, paciente y responsable.",
+          image: "img/kevin.PNG",
+          rol: "Desarrollador FrontEnd",
+        },
+        {
+          codigo: 3,
+          nombre: "Christian Gonzales",
+          descripcion:
+            "Estudiante de Ingeniería de Sistemas en la UNAB, miembro de la Escuela Latinoamericana de Inteligencia Artificial. si lo puedes imaginar lo puedes programar..",
+          image: "img/cristian.PNG",
+          rol: "Desarrollador BackEnd",
+        },
+        {
+          codigo: 4,
+          nombre: "Juan Lopez",
+          descripcion:
+            "Soy ingeniero Aeronautico, me gusta mucho programacion especialmente en Arduino, y mi hobbie es jugar ajedrez y montar bicicleta",
+          image: "img/juan.jpg",
+          rol: "Desarrollador BackEnd",
+        },
+        {
+          codigo: 5,
+          nombre: "Camilo Ramirez",
+          descripcion:
+            " Ingeniero en electrónica de la Universidad del Quindio, tengo 25 años, soy de Armenia, Quindio y me gusta practicar deportes y pasear.",
+          image: "img/camilo.PNG",
+          rol: "Desarrolladora BackEnd",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/NewsApi.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="news">
+    <section class="container-fluid" id="news">
+      <div class="row mt-md-5 mt-sm-5 mt-xs-5">
+        <div
+          class="col-lg-6 col-xs-12"
+          v-for="(article, index) of news"
+          :key="index"
+        >
+          <div class="d-flex justify-content-center align-items-center row">
+            <div class="p-3 col-5">
+              <img
+                v-bind:src="article.urlToImage"
+                alt="Imagen noticia"
+                class="img-fluid"
+              />
+            </div>
+
+            <div class="p-2 col-7">
+              <div>
+                <h2>{{ article.title }}</h2>
+                <p class="font-italic">
+                  {{ article.description }}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="d-flex container-fluid justify-content-end pb-2 mt-n2">
+            <a
+              :href="article.url"
+              target="_blank"
+              class="btn btn-outline-success"
+              >Abrir</a
+            >
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NewsApi",
+  data() {
+    return {
+      news: null,
+    };
+  },
+
+  mounted() {
+    const testApi = async () => {
+      let urlNews = `http://newsapi.org/v2/top-headlines?country=co&category=technology&apiKey=92f7486d76354de1bc23c324ef2c2acb`;
+
+      try {
+        const result = await fetch(urlNews, {
+          method: "GET",
+          mode: "cors",
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+          },
+        });
+        const data = await result.json();
+        this.news = data.articles.slice(0, 4);
+
+        //drawMovies(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    testApi();
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
